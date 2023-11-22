2416b2297749a926bfb724617eb48a047d1e00c0
Victor Volkov <vityan2504@gmail.com>
2023-11-22 19:13:00 +0300

    Написал функцию перемещения квадрата

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 'use strict';
 
-const DomElement = function(selector, height, width, bg, fontSize) {
-   this.selector = selector;
-   this.height = height;
-   this.width = width;
-   this.bg = bg;
-   this.fontSize = fontSize;
-};
-
-DomElement.prototype.createElement = function() {
-   const name = this.selector.slice(1);
-   let newElement;
-
-   if (this.selector[0] === '.') {
-      newElement = document.createElement('div');
-      newElement.classList.add(name);
-   }
-
-   if (this.selector[0] === '#') {
-      newElement = document.createElement('p');
-      newElement.setAttribute('id', name);
-   }
-
-   newElement.style.cssText = `
-      height: ${this.height}; 
-      width: ${this.width}; 
-      background: ${this.bg}; 
-      font-size: ${this.fontSize}`;
+document.addEventListener('DOMContentLoaded', function() {
+   const DomElement = function(selector, height, width, bg, fontSize) {
+      this.selector = selector;
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;
+   };
    
-   newElement.textContent = 'Это ' + newElement.tagName;
+   DomElement.prototype.createElement = function() {
+      const name = this.selector.slice(1);
+      let newElement;
+   
+      if (this.selector[0] === '.') {
+         newElement = document.createElement('div');
+         newElement.classList.add(name);
+      }
+   
+      if (this.selector[0] === '#') {
+         newElement = document.createElement('p');
+         newElement.setAttribute('id', name);
+      }
+   
+      newElement.style.cssText = `
+         height: ${this.height}; 
+         width: ${this.width}; 
+         background: ${this.bg}; 
+         font-size: ${this.fontSize};
+         position: absolute`;
+      
+      newElement.textContent = 'Это ' + newElement.tagName;
 
-   document.body.append(newElement);
-};
+      document.body.append(newElement);
+   };
+   
+   const div = new DomElement('.block', '100px', '100px', 'red', '16px');
+   
+   div.createElement();
 
-const div = new DomElement('.block', '230px', '500px', 'red', '66px');
-const div2 = new DomElement('.block2', '300px', '100', 'green', '20px');
-const p = new DomElement('#text', '100px', '50px', 'blue', '16px');
-const p1 = new DomElement('#text2', '25px', '50px', 'yellow', '12px');
+   const sqrt = document.querySelector(div.selector);
 
+   window.addEventListener('keydown', function(e) {
+      if (e.key === "ArrowUp") {
+         sqrt.style.top = +sqrt.style.top.replace('px', '') - 10 + 'px';
+      }
+      if (e.key === "ArrowDown") {
+         sqrt.style.top = +sqrt.style.top.replace('px', '') + 10 + 'px';
+      }
+      if (e.key === "ArrowLeft") {
+         sqrt.style.left = +sqrt.style.left.replace('px', '') - 10 + 'px';
+      }
+      if (e.key === "ArrowRight") {
+         sqrt.style.left = +sqrt.style.left.replace('px', '') + 10 + 'px';
+      }
+   });
 
-div.createElement();
-p.createElement();
-div2.createElement();
-p1.createElement();
+});
